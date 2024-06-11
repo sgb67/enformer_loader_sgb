df0aa10dfb2c9d7256268ed78eed9924f16fd81c
Jacob Hepkema <jacobhepkema@gmail.com>
2024-06-11 10:04:37 +0100

    rename data.py to utils.py (data reserved for loaders etc)

--- enformer_loader/__init__.py
+++ enformer_loader/__init__.py
@@ -1,4 +1,4 @@
-from .data import get_chrom_sizes, avg_bin, sum_bin, random_region, get_bw_signal
+from .utils import get_chrom_sizes, avg_bin, sum_bin, random_region, get_bw_signal
 
 __all__ = ['get_chrom_sizes', 'avg_bin',
            'sum_bin', 'random_region', 'get_bw_signal']
